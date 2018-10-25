803801ea28ec952fdb429e13decbb56e9f8f9032
griffithzt <42622779+griffithzt@users.noreply.github.com>
2018-10-25 18:42:21 -0400

    Add files via upload

---
+++ CSCI215_Lab_06-master/lab6.js
@@ -0,0 +1,8 @@
+
+
+document.getElemebntById("name").onKeyup=function checkName(){
+		var regx =  /</
+		var reg2 = />/
+		if (name == regEx || name == regEx2){
+		 document.getElementById("name").style.backgroundColor = "red";
+		 }}
